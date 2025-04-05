4c0016ad8a081aa9d7ff665bddea5c39a61b14b7
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2025-04-05 10:00:26 +0600

    add set and tuples

--- Basic/tuple.py
+++ Basic/tuple.py
@@ -8,4 +8,12 @@ if 'tripod' in things:
 nums=([2,4,5],[7,8,9])
 print(nums[0][1])
 nums[0][1]=9099
-print(nums) # prints [[2, 4, 5], [7, 8, 
+print(nums) # prints [[2, 4, 5], [7, 8,  
+mega=[2,3,45,6,1]
+for item in mega:
+    print(item)
+
+A={1,2,3,4}
+B={3,4,5,1}
+print(A & B)
+print(A|B) 
