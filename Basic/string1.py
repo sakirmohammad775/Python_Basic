2648950fb45baaab0f97df8fe6c5d24f520b87b8
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2025-04-05 09:51:22 +0600

    add set and tuples

---
+++ Basic/string1.py
@@ -0,0 +1,8 @@
+name1 = """
+    sakib
+    khan
+"""
+    
+print(name1[-3]) 
+print(name1[::-1])
+print(name1.upper())
---
+++ Basic/tuple.py
@@ -0,0 +1,11 @@
+def multiple():
+    return 5,4
+print(multiple()) # prints (5, 4)
+things='pen','tripod','water','pencil'
+print((things[-1]))
+if 'tripod' in things:
+    print('tripod is in things')
+nums=([2,4,5],[7,8,9])
+print(nums[0][1])
+nums[0][1]=9099
+print(nums) # prints [[2, 4, 5], [7, 8, 
