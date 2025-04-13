68983b2723d89866489eb47da080dd4e946f2489
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2025-04-13 12:09:36 +0600

    add incapsulation,inheritance

--- OOP basic/constructor.py
+++ OOP basic/constructor.py
@@ -1,21 +1,46 @@
+
 class shop:
-    products = []
 
     def __init__(self, name):
         self.name = name
+        self.products = []
+        self.__balance = 500 # private attribute
+        self._owner='Mr. X '
 
     def add_product(self, name, price):
-        product=Product(name,price)
+        product = Product(name, price)
         self.products.append(product)
 
+    def get_balance(self):
+        return self.__balance
+    
+    def __str__(self):
+        return f"This is a shop with the name {self.name}"
+
+class subShop(shop):
+    pass
 
 class Product:
     def __init__(self, name, price):
         self.name = name
         self.price = price
 
+    def __repr__(self):
+        return self.name
+
 
 shop1 = shop("top class")
-shop1.add_product('mobile',40000)
-shop1.add_product('laptop',50000)
-print(shop1.products)
+shop1.add_product("mobile", 40000)
+shop1.add_product("laptop", 50000)
+
+shop2 = shop("top class 2")
+shop2.add_product("keyboard", 40000)
+shop2.add_product("mouse", 50000)
+
+print(shop2._owner)
+
+"""
+brac bank [class attribute]
+- account number
+- account holder name [ instance attribute]
+"""
---
+++ OOP basic/inheritance.py
@@ -0,0 +1,28 @@
+class Vehicle:
+    def __init__(self, brand, model):
+        self.brand=brand
+        self.model=model
+    def speed(self):
+        pass
+        
+
+class Car(Vehicle):
+    def __init__(self,brand,model,capacity):
+        super().__init__(brand,model)
+        self.capacity=capacity
+
+class ElectricalCar(Car):
+    def __init__(self, brand, model, capacity,battery_capacity):
+        super().__init__(brand, model, capacity)
+        self.battery_capacity=battery_capacity
+
+class Bike:
+    def __init__(self,brand,model,milage):
+        self.milage=milage
+        
+        
+bmw=Car("BMW","AB",7)
+# print(bmw.brand,bmw.model,bmw.capacity)
+
+tesla=ElectricalCar("Tesla","XB",7,10000)
+print(tesla.brand,tesla.model,tesla.capacity,tesla.battery_capacity)
